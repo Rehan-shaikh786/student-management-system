2bb41e19df5765fc60a0f0b44618ba0a565de598
Rehan-shaikh786 <rs9212370@gmail.com>
2026-09-24 13:19:05 +0530

    Implement student create operation

--- src/main/java/com/student/main/Main.java
+++ src/main/java/com/student/main/Main.java
@@ -2,7 +2,6 @@ package com.student.main;
 
 import com.student.dao.StudentDao;
 import com.student.dao.StudentDaoImpl;
-import com.student.entity.Student;
 
 public class Main {
 
@@ -10,28 +9,7 @@ public class Main {
 
         StudentDao studentDao = new StudentDaoImpl();
 
-        // Find existing student
-        Student student = studentDao.getStudentById(2);
-
-        if (student != null) {
-
-            // Update student details
-            student.setName("Rahul Pawar");
-            student.setEmail("rahulpawar@gmail.com");
-            student.setPhone("9876501234");
-            student.setAge(23);
-            student.setCity("Satara");
-            student.setAddress("Satara, Maharashtra");
-
-            // Save updated student
-            studentDao.updateStudent(student);
-
-            System.out.println("Updated Student:");
-            System.out.println(student);
-
-        } else {
-
-            System.out.println("Student not found.");
-        }
+        // Delete student with ID 2
+        studentDao.deleteStudent(2);
     }
 }
